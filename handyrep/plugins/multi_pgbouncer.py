d30559dea6558bd22ef1e58fe0a1a2f33bcdf347
B. Heath Robinson <heath@midnighthour.org>
2014-11-19 17:46:17 +0000

    Add ability to specify connection params per database

--- handyrep/plugins/multi_pgbouncer.py
+++ handyrep/plugins/multi_pgbouncer.py
@@ -108,8 +108,12 @@ class multi_pgbouncer(HandyRepPlugin):
 
     def test(self):
         #check that we have all config variables required
-        if self.failed( self.test_plugin_conf("multi_pgbouncer","pgbouncerbin","template","owner","config_location","database_list","readonly_suffix","all_replicas")):
+        if self.failed( self.test_plugin_conf("multi_pgbouncer","pgbouncerbin","template","owner","config_location","readonly_suffix","all_replicas")):
             return self.rd(False, "multi-pgbouncer failover is not configured" )
+
+        if self.failed( self.test_plugin_conf("multi_pgbouncer","database_list") or self.test_plugin_conf("multi_pgbouncer","databases")):
+            return self.rd(False, "multi-pgbouncer failover has no configured databases" )
+
         #check that we can connect to the pgbouncer servers
         blist = self.bouncer_list()
         if len(blist) == 0:
@@ -157,23 +161,31 @@ class multi_pgbouncer(HandyRepPlugin):
         # servers for pgbouncer
         # build master string first
         myconf = self.conf["plugins"]["multi_pgbouncer"]
-        dblist = myconf["database_list"]
+        if myconf["databases"]:
+            dbconfig = myconf["databases"]
+
+        if myconf["database_list"]:
+            dbconfig = {}
+            for dbname in myconf["database_list"]:
+                dbconfig[dbname] = myconf["extra_connect_param"]
+
         # add in the handyrep db if the user has forgotten it
-        if self.conf["handyrep"]["handyrep_db"] not in dblist:
-            dblist.append(self.conf["handyrep"]["handyrep_db"])
-        constr = self.dbconnect_line(dblist, self.servers[master]["hostname"], self.servers[master]["port"], "", myconf["extra_connect_param"])
+        if not dbconf.has_key(self.conf["handyrep"]["handyrep_db"]):
+            dbconf[self.conf["handyrep"]["handyrep_db"]] = myconf["extra_connect_param"]
+
+        constr = self.dbconnect_line(dbconfig, self.servers[master]["hostname"], self.servers[master]["port"], "")
         replicas = self.sorted_replicas()
         if self.is_true(myconf["all_replicas"]):
             #if we're doing all replicas, we need to put them in as _ro0, _ro1, etc.
             # if there's no replicas, set ro1 to go to the master:
             if len(replicas) == 0 or (len(replicas) == 1 and master in replicas):
                 rsuff = "%s%d" % (myconf["readonly_suffix"],1,)
-                constr += self.dbconnect_line(myconf["database_list"], self.servers[master]["hostname"], self.servers[master]["port"], rsuff, myconf["extra_connect_param"])
+                constr += self.dbconnect_line(dbconfig, self.servers[master]["hostname"], self.servers[master]["port"], rsuff)
             else:
                 for rep in replicas:
                     if not rep == master:
                         rsuff = "%s%d" % (myconf["readonly_suffix"],repno,)
-                        constr += self.dbconnect_line(myconf["database_list"], self.servers[rep]["hostname"], self.servers[rep]["port"], rsuff, myconf["extra_connect_param"])
+                        constr += self.dbconnect_line(dbconfig, self.servers[rep]["hostname"], self.servers[rep]["port"], rsuff)
                         repno += 1
         else:
             # only one readonly replica, setting it up with _ro
@@ -183,23 +195,17 @@ class multi_pgbouncer(HandyRepPlugin):
                     replicas.pop(0)
                     
             if len(replicas) > 0:
-                constr += self.dbconnect_line(myconf["database_list"], self.servers[replicas[0]]["hostname"], self.servers[replicas[0]]["port"], myconf["readonly_suffix"], myconf["extra_connect_param"])
+                constr += self.dbconnect_line(dbconfig, self.servers[replicas[0]]["hostname"], self.servers[replicas[0]]["port"], myconf["readonly_suffix"])
             else:
                 # if no replicas, read-only connections should go to the master
-                constr += self.dbconnect_line(myconf["database_list"], self.servers[master]["hostname"], self.servers[master]["port"], myconf["readonly_suffix"], myconf["extra_connect_param"])
+                constr += self.dbconnect_line(dbconfig, self.servers[master]["hostname"], self.servers[master]["port"], myconf["readonly_suffix"])
 
         return constr
 
 
-    def dbconnect_line(self, database_list, hostname, portno, suffix, extra):
+    def dbconnect_line(self, database_list, hostname, portno, suffix):
         confout = ""
-        if extra:
-            nex = extra
-        else:
-            nex = ""
-        for dbname in database_list:
+        for dbname,nex in database_list.items():
             confout += "%s%s = dbname=%s host=%s port=%s %s \n" % (dbname, suffix, dbname, hostname, portno, nex,)
 
         return confout
-
-    
